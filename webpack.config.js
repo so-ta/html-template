52494611750ecb96fbfb0cb328eefc0f99e839dc
souta <souta@soutanoMacBook-puro.local>
2019-07-08 23:06:44 +0900

    update webpack and dockerfile

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path'),
-    ExtractTextPlugin = require('extract-text-webpack-plugin'),
-    webpack = require('webpack'),
-    cssnano = require('cssnano');
+  ExtractTextPlugin = require('extract-text-webpack-plugin'),
+  CopyWebpackPlugin = require('copy-webpack-plugin'),
+  webpack = require('webpack'),
+  cssnano = require('cssnano');
 
 const workDirectory = path.join(__dirname);
 
@@ -9,81 +10,91 @@ const workDirectory = path.join(__dirname);
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = [{
-    mode: isProduction ? 'production' : 'development',
-    entry: {
-        'index': path.join(workDirectory, 'src', 'scripts', 'entry.js')
-    },
-    output: {
-        path: path.join(workDirectory, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    query: {
-                        compact: isProduction
-                    }
-                }
-            }
-        ]
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery',
-            "window.$": "jquery",
-        })
-    ],
-}, {
-    mode: isProduction ? 'production' : 'development',
-    entry: {
-        'style': path.join(workDirectory, 'src', 'styles', 'entry.scss')
-    },
-    output: {
-        path: path.join(workDirectory, 'public'),
-        filename: '[name].css'
-    },
-    devtool: isProduction ? '' : 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                sourceMap: !isProduction,
-                                url: false,
-                            }
-                        }, {
-                            loader: 'postcss-loader',
-                            options: {
-                                sourceMap: !isProduction,
-                                plugins: [
-                                    cssnano({
-                                        preset: 'default',
-                                    })
-                                ]
-                            },
-                        },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                sourceMap: !isProduction,
-                            }
-                        }
-                    ],
-                })
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin('[name].css'),
+  mode: isProduction ? 'production' : 'development',
+  watch: !isProduction,
+  entry: {
+    'index': path.join(workDirectory, 'src', 'scripts', 'entry.js')
+  },
+  output: {
+    path: path.join(workDirectory, 'public', 'scripts'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            compact: isProduction
+          }
+        }
+      }
     ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      "window.$": "jquery",
+    })
+  ],
+}, {
+  mode: isProduction ? 'production' : 'development',
+  watch: !isProduction,
+  entry: {
+    'style': path.join(workDirectory, 'src', 'styles', 'entry.scss')
+  },
+  output: {
+    path: path.join(workDirectory, 'public', 'styles'),
+    filename: '[name].css'
+  },
+  devtool: isProduction ? '' : 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: !isProduction,
+                url: false,
+              }
+            }, {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: !isProduction,
+                plugins: [
+                  cssnano({
+                    preset: 'default',
+                  })
+                ]
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: !isProduction,
+              }
+            }
+          ],
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css'),
+    new CopyWebpackPlugin([{
+      from: path.join(workDirectory, 'src', 'images'),
+      to: path.join(workDirectory, 'public', 'images')
+    }]),
+    new CopyWebpackPlugin([{
+      from: path.join(workDirectory, 'views'),
+      to: path.join(workDirectory, 'public')
+    }]),
+  ]
 }];
